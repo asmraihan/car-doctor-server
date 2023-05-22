ae6fa329e732e594eb36d04a31ffd2f17e77b18c
asmraihan <asmraihanctg14@gmail.com>
2023-05-22 13:54:24 +0600

    sorting

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mznotex.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -42,7 +41,7 @@ const verifyJWT =(req, res, next)=>{
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services')
         const bookingCollection = client.db('carDoctor').collection('bookings')
@@ -59,7 +58,17 @@ async function run() {
 
         // services routes
         app.get('/services', async (req, res) => {
-            const cursor = serviceCollection.find()
+            const sort = req.query.sort
+            // const query = {}
+            const query = { price: { $gte: 50, $lte: 150 } };
+            const options = {
+                // sort matched documents in descending order by rating
+                sort: { 
+                    "price": sort === 'asc' ? 1 : -1
+                }, 
+              };
+
+            const cursor = serviceCollection.find(query, options)
             const result = await cursor.toArray()
             res.send(result)
         })
